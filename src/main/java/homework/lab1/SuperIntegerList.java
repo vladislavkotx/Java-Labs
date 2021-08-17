37e899e1837e8835a05d160db00cae268a2becc0
vlad <vladislavkotx@gmail.com>
2021-08-17 10:24:13 +0300

    lab1

---
+++ src/main/java/homework/lab1/SuperIntegerList.java
@@ -0,0 +1,18 @@
+package homework.lab1;
+
+/**
+ * @author Evgeny Borisov
+ */
+
+public interface SuperIntegerList {
+
+    void add(int number);
+
+    void removeByIndex(int index);
+
+    void removeByValue(int value);
+
+    int get(int index);
+
+    void printAll();
+}
