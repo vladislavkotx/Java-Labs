349ae3275b0c0aaa050f749bb9c1fe2f546b65ca
vlad <vladislavkotx@gmail.com>
2021-08-23 14:43:00 +0300

    NUS adds

--- src/main/java/classwork/design_patterns/never_use_switch/Main.java
+++ src/main/java/classwork/design_patterns/never_use_switch/Main.java
@@ -1,8 +1,13 @@
 package classwork.design_patterns.never_use_switch;
 
+import classwork.design_patterns.never_use_switch.mail_sender_service.HappyBDayMailSenderService;
+import classwork.design_patterns.never_use_switch.mail_sender_service.MailSenderService;
+import classwork.design_patterns.never_use_switch.mail_sender_service.WelcomeMailSenderSevice;
 import com.github.javafaker.Faker;
 import lombok.SneakyThrows;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Main {
@@ -10,6 +15,7 @@ public class Main {
     @SneakyThrows
     public static void main(String[] args) {
         MailSender mailSender = new MailSender();
+//        mailSender.fillMap();
         Faker faker = new Faker();
         while (true) {
             MailInfo mailInfo = MailInfo.builder()
@@ -22,3 +28,10 @@ public class Main {
         }
     }
 }
+
+//    private final Map<Integer, MailSenderService> mailMap = new HashMap<>();
+
+//    public void fillMap () {
+//        mailMap.put(1, new WelcomeMailSenderSevice());
+//        mailMap.put(2, new HappyBDayMailSenderService());
+//    }
