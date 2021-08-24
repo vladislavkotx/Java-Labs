b6fd1f675caf3c4c10607d9584d9dbed03437737
vlad <vladislavkotx@gmail.com>
2021-08-24 13:13:37 +0300

    employee 3 tasks

--- src/main/java/classwork/java8/employees_salaries/Employee.java
+++ src/main/java/classwork/java8/employees_salaries/Employee.java
@@ -3,7 +3,10 @@ package classwork.java8.employees_salaries;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -11,6 +14,23 @@ public class Employee {
     private String name;
     private int salary;
 
+    public static int employeeSum(List <Employee> list) {
+        return list.stream()
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+
+    public static List <String> sortedBySalary (List <Employee> list) {
+        return list.stream()
+                .sorted(Comparator.comparing(Employee::getSalary))
+                .map(Employee::getName)
+                .collect(Collectors.toList());
+    }
+
+    public static Map <String, Employee> mapNameToEmployee (List <Employee> list) {
+        return list.stream()
+                .collect(Collectors.toMap(Employee::getName, t -> t, (t1, t2) -> (t1.getSalary() > t2.getSalary() ? t1 : t2)));
+    }
 
     public static void main(String[] args) {
         Employee max = Employee.builder()
@@ -24,10 +44,14 @@ public class Employee {
                 .build();
 
         List<Employee> list = List.of(max, alex);
-        int sumSalary = list.stream()
-                .mapToInt(Employee::getSalary)
-                .sum();
-        System.out.println(sumSalary);
+        int sum = employeeSum(list);
+        System.out.println(sum);
+
+        List<String> strings = sortedBySalary(list);
+        System.out.println(strings);
+
+        Map<String, Employee> stringEmployeeMap = mapNameToEmployee(list);
+        System.out.println(stringEmployeeMap);
     }
 
 }
